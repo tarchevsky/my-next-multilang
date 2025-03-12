a26436b9d4e9b673d954ac073ed86ec8eb555e1d
tarchevskyivan <i.tarchevsky@gmail.com>
2025-03-12 12:14:00 +0300

    Переписал в hero, чтобы был не img, а Image
    и указал сразу quality

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,14 +1,18 @@
 import { HeroProps } from '@/types'
+import Image from 'next/image'
 
 const Hero = ({ title, buttonText, src, alt }: HeroProps) => {
 	return (
 		<main className='hero md:min-h-[80vh]'>
 			<div className='relative hero-content flex-col lg:flex-row'>
-				<img
+				<Image
 					className='lg:w-1/2 h-[600px] object-cover rounded-box shadow-2xl'
 					src={src}
-					alt={alt}
+					alt={alt || 'Картинка, назначение не написано'}
 					loading='lazy'
+					width={800}
+					height={600}
+					quality={75}
 				/>
 				<div className='absolute lg:relative lg:ml-[-100px] z-10'>
 					{title ? (
